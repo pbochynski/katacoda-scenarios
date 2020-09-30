7191569934133eb16f9493d72fcd10ea602e1d60
Piotr Bochynski <piotr.bochynski@sap.com>
2020-09-30 22:32:25 +0200

    Add logs

--- kyma-serverless/assets/wait-for-function.sh
+++ kyma-serverless/assets/wait-for-function.sh
@@ -1,5 +1,7 @@
 RUNNING=""
-while [[ $RUNNING != "True" ]]; do RUNNING=$(kubectl get function hello -ojsonpath='{.status.conditions[?(@.type=="Running")].status}'); echo "waiting for function hello, pods:\n$(kubectl get pods)"; sleep 2; done
+SECONDS=0
+while [[ $RUNNING != "True" && SECONDS<60]]; do RUNNING=$(kubectl get function hello -ojsonpath='{.status.conditions[?(@.type=="Running")].status}'); echo "waiting for function hello, pods:\n$(kubectl get pods)"; sleep 2; done
+kubectl logs -l serverless.kyma-project.io/function-name=hello
 kubectl port-forward --address 0.0.0.0 svc/hello 8080:80 &
 MSG=""
 while [[ "$MSG" != "Hello World!" ]]; do MSG=$(curl -s -m 5 http://localhost:8080); echo "MSG from service: $MSG"; sleep 2; done
