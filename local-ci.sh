b8fc8c4b688f52349f656ee5b748c6480ef8a788
Piotr Bochynski <piotr.bochynski@sap.com>
2020-10-01 01:09:12 +0200

    Ignore local-ci.sh

--- local-ci.sh
+++
@@ -1,6 +0,0 @@
-k3d cluster create test
-k3d kubeconfig get test >kubeconfig
-export REGISTRY_USER=
-export REGISTRY_PASS=
-act -P ubuntu-latest=nektos/act-environments-ubuntu:18.04 -s REGISTRY_USER -s REGISTRY_PASS
-k3d cluster delete test
