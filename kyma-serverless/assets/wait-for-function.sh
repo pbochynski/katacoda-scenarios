e44cbf291d55b0c551d5c6ea55e0154d24a4936b
Piotr Bochynski <piotr.bochynski@sap.com>
2020-09-30 22:24:17 +0200

    Add pod info

--- kyma-serverless/assets/wait-for-function.sh
+++ kyma-serverless/assets/wait-for-function.sh
@@ -1,5 +1,5 @@
 RUNNING=""
-while [[ $RUNNING != "True" ]]; do RUNNING=$(kubectl get function hello -ojsonpath='{.status.conditions[?(@.type=="Running")].status}'); echo "waiting for function hello, pods: $(kubectl get pods -ojsonpath='{.items[*].metadata.name}')"; sleep 2; done
+while [[ $RUNNING != "True" ]]; do RUNNING=$(kubectl get function hello -ojsonpath='{.status.conditions[?(@.type=="Running")].status}'); echo "waiting for function hello, pods:\n$(kubectl get pods)"; sleep 2; done
 kubectl port-forward --address 0.0.0.0 svc/hello 8080:80 &
 MSG=""
 while [[ "$MSG" != "Hello World!" ]]; do MSG=$(curl -s -m 5 http://localhost:8080); echo "MSG from service: $MSG"; sleep 2; done
