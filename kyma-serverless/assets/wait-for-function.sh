c4c7093ea47843cda8267e4fcb1d7cc62e440de1
Piotr Bochynski <piotr.bochynski@sap.com>
2020-09-30 23:01:31 +0200

    Improve wait script

--- kyma-serverless/assets/wait-for-function.sh
+++ kyma-serverless/assets/wait-for-function.sh
@@ -1,6 +1,6 @@
 RUNNING=""
 SECONDS=0
-while [[ $RUNNING != "True" && SECONDS<60]]; do RUNNING=$(kubectl get function hello -ojsonpath='{.status.conditions[?(@.type=="Running")].status}'); echo "waiting for function hello, pods:\n$(kubectl get pods)"; sleep 2; done
+while [[ "$RUNNING" == "True" ]] && [[ $SECONDS -le 60 ]]; do RUNNING=$(kubectl get function hello -ojsonpath='{.status.conditions[?(@.type=="Running")].status}'); echo "waiting for function hello, pods:\n$(kubectl get pods)"; sleep 2; done
 kubectl logs -l serverless.kyma-project.io/function-name=hello
 kubectl port-forward --address 0.0.0.0 svc/hello 8080:80 &
 MSG=""
