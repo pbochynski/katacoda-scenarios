59128d4fe7464ae949cae65bc04cf9e2408e59b7
pbochynski <p.bochynski@gmail.com>
2022-10-03 15:31:01 +0200

    Local registry

--- kyma-basics/kyma-init.sh
+++ kyma-basics/kyma-init.sh
@@ -2,10 +2,3 @@ echo "Download Kyma CLI"
 curl -Lo kyma https://storage.googleapis.com/kyma-cli-stable/kyma-linux
 chmod +x kyma
 mv kyma /usr/local/bin
-echo "Start local docker registry"
-docker run -d \
--p 5000:5000 \
---restart=always \
---name registry.localhost \
--v $PWD/registry:/var/lib/registry \
-eu.gcr.io/kyma-project/test-infra/docker-registry-2:20200202
