1c9e3c5c93850fd261040f72cf74e9d33aecd0b6
pbochynski <p.bochynski@gmail.com>
2022-10-19 13:57:05 +0200

    Waiting for CRD

--- kyma-basics/kyma-init.sh
+++ kyma-basics/kyma-init.sh
@@ -1,5 +1,7 @@
 kubectl create ns istio-operator
 kubectl create ns istio-system
 kubectl create ns busola
+kubectl apply -f istio-operator.yaml
+kubectl wait --for condition=established --timeout=60s crd/istiooperators.install.istio.io
 kubectl apply -f istio.yaml
 kubectl apply -f busola.yaml
