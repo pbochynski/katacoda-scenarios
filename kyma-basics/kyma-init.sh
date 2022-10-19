43a6a6fa00e607f790e88b29491a57279432f89b
pbochynski <p.bochynski@gmail.com>
2022-10-19 14:52:00 +0200

    Better waiting message

--- kyma-basics/kyma-init.sh
+++ kyma-basics/kyma-init.sh
@@ -6,6 +6,6 @@ kubectl wait --for condition=established --timeout=60s crd/istiooperators.instal
 kubectl apply -f istio.yaml -n istio-system
 kubectl apply -f busola.yaml -n busola
 while [[ -z $(kubectl get svc -n istio-system istio-ingressgateway 2>/dev/null) ]]; do
-  echo "Waiting for istio-ingressgateway"
-  sleep 1
+  echo "Waiting for istio-ingressgateway (usually about 30-60s)"
+  sleep 3
 done
