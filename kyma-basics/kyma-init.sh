0d617fb84fbf8f4d29bdb8d9758ebfd17214203c
pbochynski <p.bochynski@gmail.com>
2022-10-19 14:02:31 +0200

    Add namespaces

--- kyma-basics/kyma-init.sh
+++ kyma-basics/kyma-init.sh
@@ -3,5 +3,5 @@ kubectl create ns istio-system
 kubectl create ns busola
 kubectl apply -f istio-operator.yaml
 kubectl wait --for condition=established --timeout=60s crd/istiooperators.install.istio.io
-kubectl apply -f istio.yaml
-kubectl apply -f busola.yaml
+kubectl apply -f istio.yaml -n istio-system
+kubectl apply -f busola.yaml -n busola
